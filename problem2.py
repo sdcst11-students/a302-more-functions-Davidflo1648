02006f21a234500a0f3ac3c9bf9da126614db669
Davidflo1648 <144282155+Davidflo1648@users.noreply.github.com>
2023-10-30 12:38:06 -0700

    problem2 done

--- problem2.py
+++ problem2.py
@@ -20,9 +20,20 @@ assert triangle(5,15,12) == 3
 assert triangle(1,1,4) == 0  
 (2 points)
 """
+import math
+def triangle(a, b, c):
+  if a + b <= c or a + c <= b or b + c <= a:
+    return "Invalid triangle"
+  A = math.acos((b**2 + c**2 - a**2) / (2 * b * c))
+  B = math.acos((a**2 + c**2 - b**2) / (2 * a * c))
+  C = math.acos((a**2 + b**2 - c**2) / (2 * a * b))
+  if A != B and B != C and C != A:
+    return "Scalene triangle"
+  elif A == math.pi/2 or B == math.pi/2 or C == math.pi/2:
+    return "Right triangle"
+  else:
+    return "Obtuse triangle"
 
-def triangle():
-    return
 
 def tests():
     assert triangle(12,5,13) == 2     
